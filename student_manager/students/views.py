120924e3187d449d9e71204da166fb24a37b93e9
Tonmoy <tonmoykrpaul@gmail.com>
2025-03-16 18:02:48 +0600

    Initial commit: Student Management System with .gitignore

---
+++ student_manager/students/views.py
@@ -0,0 +1,43 @@
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib import messages
+from .models import Student
+from .forms import StudentForm
+
+class StudentListView(ListView):
+    model = Student
+    template_name = 'students/student_list.html'
+    context_object_name = 'students'
+
+class StudentDetailView(DetailView):
+    model = Student
+    template_name = 'students/student_detail.html'
+
+class StudentCreateView(CreateView):
+    model = Student
+    form_class = StudentForm
+    template_name = 'students/student_form.html'
+    success_url = reverse_lazy('student-list')
+
+    def form_valid(self, form):
+        messages.success(self.request, 'Student added successfully!')
+        return super().form_valid(form)
+
+class StudentUpdateView(UpdateView):
+    model = Student
+    form_class = StudentForm
+    template_name = 'students/student_form.html'
+    success_url = reverse_lazy('student-list')
+
+    def form_valid(self, form):
+        messages.success(self.request, 'Student updated successfully!')
+        return super().form_valid(form)
+
+class StudentDeleteView(DeleteView):
+    model = Student
+    template_name = 'students/student_confirm_delete.html'
+    success_url = reverse_lazy('student-list')
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, 'Student deleted successfully!')
+        return super().delete(request, *args, **kwargs)
